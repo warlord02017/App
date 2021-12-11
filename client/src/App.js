9c868f8e08d459927b3c3639bf580d18a5dd0b06
Parth Patel <parthpatel@Parths-MacBook-Pro.local>
2021-12-11 16:42:51 -0500

    remove main' '

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 import Head2HeadTeams from './pages/Head2HeadTeams';
 import Head2HeadTeamsStats from './pages/Head2HeadTeamsStats';
 import Head2HeadPlayers from './pages/Head2HeadPlayers';
-import Main from './pages/Main';
 import Teams from './pages/Teams';
 import Batters from './pages/Batters';
 import Pitchers from './pages/Pitchers';
@@ -18,7 +17,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Main />}></Route>
+        <Route path="/" element={<Head2HeadTeams />}></Route>
         <Route path="/Head2Head/Teams" element={<Head2HeadTeams />}></Route>   
         <Route path="/Head2Head/Players" element={<Head2HeadPlayers />}></Route>
         <Route path="/Players" element={<Players />}></Route>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="/">MLBanalytics</Navbar.Brand>
+  <Navbar.Brand href="/Head2Head/Teams">MLBanalytics</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
